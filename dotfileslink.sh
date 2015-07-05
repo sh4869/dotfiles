00c49e14dfdd982afeb99784893f366738b8ea9c
sh4869 <nobuk4869@gmail.com>
2015-07-05 14:15:31 +0900

    Create setting_bashrc function

--- dotfileslink.sh
+++ dotfileslink.sh
@@ -39,6 +39,7 @@ setting_vimrc(){
 		if type git > /dev/null
 		then 
 			#SetUp Neobundle
+			echo "Installing NeoBundle....."	
 			mkdir -p ~/.vim/bundle
 			git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
@@ -46,16 +47,49 @@ setting_vimrc(){
 			chmod +r -R ~/.vim/
 			chmod +w -R ~/.vim/
 
-			echo "> Install Neobundle.vim"
-			echo "> And Change mode ~/.vim dir"
+			echo "Install Neobundle.vim"
+			echo "And Change mode ~/.vim dir"
 		else
-			echo "> Please Install Git for setting Neobundle"
+			echo "Please Install Git for setting Neobundle"
 		fi	
 	else
 		echo  "Neobundle Alredy Exists"
 	fi
 }
+
+setting_bash(){
+	echo "[.bashrc Setting]"
+	#Setting of .bash
+	if [ -e ~/.bashrc ]
+	then
+		while true;do
+			echo ">> Do you remove .bashrc and replace .bashrc? [y/n]"
+			read answer
+			case $answer in
+				y)
+					rm ~/.bashrc 
+					echo "Remove .bashrc"
+					ln -s ~/.dotfiles/.bashrc ~/.bashrc
+					echo "And Replace .bashrc!!"
+					break
+					;;
+				n)
+					echo "Don't remove .bashrc"
+					break
+					;;
+				*)
+					echo "Plese input y or n"
+					;;
+			esac
+		done
+	else
+		ln -s ~/.dotfiles/.bashrc ~/.bashrc
+		echo "Replce .bashrc!!"
+	fi
+}
+
 setting_vimrc
+setting_bash
 
 #rm ~/.vimrc ~/.bashrc ~/.gitconfig
 #ln -s ~/.dotfiles/.vimrc ~/.vimrc

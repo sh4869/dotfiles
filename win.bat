92f2bd3d3536fe4266fd86cedfe73304fce0a0d8
sh4869 <nobuk4869@gmail.com>
2016-09-27 11:39:33 +0900

    Add dein.toml

--- win.bat
+++ win.bat
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
-
+mklink %HOMEPATH%\.vim\rc\dein.toml %HOMEPATH%\dotfiles\vim\dein.toml
+mklink %HOMEPATH%\.vim\rc\dein_lazy.toml %HOMEPATH%\dotfiles\vim\dein_lazy.toml
 

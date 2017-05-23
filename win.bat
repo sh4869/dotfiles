430454e937b46e7eccd5228e739d283787e9e3af
sh4869 <nobuk4869@gmail.com>
2017-05-23 13:00:33 +0900

    fix windows install bat file

--- win.bat
+++ win.bat
@@ -1,5 +1,9 @@
+@echo off
+if not exist "%HOMEPATH%\.vim" mkdir "%HOMEPATH%\.vim"
+if not exist "%HOMEPATH%\.vim\rc" mkdir "%HOMEPATH%\.vim\rc"
 mklink %HOMEPATH%\_vimrc %HOMEPATH%\dotfiles\.vimrc
 mklink %HOMEPATH%\.vim\rc\dein.toml %HOMEPATH%\dotfiles\vim\dein.toml
 mklink %HOMEPATH%\.vim\rc\dein_lazy.toml %HOMEPATH%\dotfiles\vim\dein_lazy.toml
-mklink /D %HOMEPATH%\AppData\Roaming\Code\User\snippets %HOMEPATH%\dotfiles\vscode\snippets
-
+if not exist "%HOMEPATH%\vimfiles" mkdir "%HOMEPATH%\vimfiles"
+if not exist "%HOMEPATH%\vimfiles\colors" mkdir "%HOMEPATH%\vimfiles\colors"
+where /Q curl && curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > %HOMEPATH%\vimfiles\colors\molokai.vim

2e3b2ec917b36b641409788cd73a9a3f9ceaaf82
sh4869 <nobuk4869@gmail.com>
2022-07-27 23:03:55 +0900

    update theme color

--- tmux/mytheme.sh
+++ tmux/mytheme.sh
@@ -39,18 +39,18 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
   TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
     #"earthquake 3 0" \
-    "pwd 89 211" \
+    "pwd 89 0" \
     #"macos_notification_count 29 255" \
     #"mailcount 9 255" \
     #"now_playing 234 37" \
     #"cpu 240 136" \
     #"load 237 167" \
     #"tmux_mem_cpu_load 234 136" \
-    "battery 137 127" \
+    # "battery 137 127" \
     #"weather 37 255" \
     #"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
     #"xkb_layout 125 117" \
-    "date_day 235 136" \
+    "date_day 235 136 " \
     "date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
     "time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
     #"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \

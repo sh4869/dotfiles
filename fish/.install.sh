5bbea2141c93f5b95d9465b458598b267a8a4de6
sh4869 <nobuk4869@gmail.com>
2022-12-26 17:55:16 +0900

    fishも管理するように

---
+++ fish/.install.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+mkdir -p ~/.config
+ln -s `pwd`/fish/fish ~/.config/
---
+++ fish/fish/config.fish
@@ -0,0 +1,8 @@
+status --is-interactive; and source (nodenv init - --no-rehash|psub)
+#status --is-interactive; and source (pyenv init - --no-rehash|psub)
+eval (pyenv init --path)
+status --is-interactive; and source (rbenv init - --no-rehash|psub)
+set PATH /usr/local/opt/gettext/bin $PATH
+alias tate='/Applications/TateShikiForMac.app/Contents/MacOS/TateShikiForMac'
+alias gvim='/Applications/MacVim.app/Contents/bin/gvim'
+
